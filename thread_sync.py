e5f459db410f1a50bc4514d8c40f0f9658a5932b
MikuGhoul <mikughoull@gmail.com>
2017-12-13 15:15:14 +0800

    thread_sync implement by Condition

--- thread_sync.py
+++ thread_sync.py
@@ -6,32 +6,28 @@ buffers = []
 for i in range(20):
     buffers.append(0)
 
-_wait_queue = {}
+condition = threading.Condition()
 
 class Semaphore:
     def __init__(self, sem):
         self._sem = sem
-        # self.lock = threading.Lock()
+
     def P(self):
-        global _wait_queue
         self._sem -= 1
-        if self._sem < 0:
-            # block当前线程并加入等待队列
-            _lock = threading.Lock()
-            _wait_queue[threading.current_thread()] = _lock
-            #print ('locking')
-            _lock.acquire()
-            _lock.acquire()
-            #print ('locked')
+        if condition.acquire():
+            if self._sem < 0:
+                # block当前线程并加入等待队列(wait池)
+                condition.wait()
+            condition.release()
+            
             
     def V(self):
         self._sem += 1
-        if self._sem <= 0:
-            # 从等待队列中随机取出一个block线程并唤醒
-            locked_thread = _wait_queue.popitem()
-            #print ('wakeuping')
-            locked_thread[1].release()
-            #print ('wakeup done')
+        if condition.acquire():
+            if self._sem <= 0:
+                # 从等待队列中随机取出一个block线程并唤醒
+                condition.notify()
+            condition.release()
 
 
 
@@ -76,7 +72,7 @@ class BoundedBuffer:
 if __name__ == '__main__':
     bf = BoundedBuffer()
     producer_container = [threading.Thread(target = bf.producer) for i in range(4)]
-    consumer_container = [threading.Thread(target = bf.consumer) for i in range(2)]
+    consumer_container = [threading.Thread(target = bf.consumer) for i in range(3)]
 
     for _thread in producer_container + consumer_container:
         _thread.start()
